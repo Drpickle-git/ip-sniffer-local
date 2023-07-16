637c6dfb3111723b4719298a7f9acac7e8a348c2
SpgBeats <129784141+SpgBeats@users.noreply.github.com>
2023-07-16 14:34:47 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,23 @@
+from scapy.all import ARP, Ether, srp
+
+def scan_network(target_ip, target_mac):
+    arp_request = ARP(pdst=target_ip)
+    ether = Ether(dst=target_mac)
+    packet = ether / arp_request
+
+    result = srp(packet, timeout=3, verbose=0)[0]
+
+    devices = []
+    for sent, received in result:
+        devices.append({'ip': received.psrc, 'mac': received.hwsrc})
+    return devices
+
+if __name__ == "__main__":
+    target_ip = "192.168.1.1/24"  # Plage d'adresses IP à scanner (généralement x.x.x.1/24)
+    target_mac = "ff:ff:ff:ff:ff:ff"  # Adresse MAC de diffusion (broadcast)
+
+    devices = scan_network(target_ip, target_mac)
+
+    print("Appareils trouvés sur le réseau :")
+    for device in devices:
+        print(f"IP : {device['ip']}, MAC : {device['mac']}")
